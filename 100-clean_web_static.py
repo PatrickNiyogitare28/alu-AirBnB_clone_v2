47559113cd7246fe7e1edddfb365795d94b7a14d
Patrick Niyo <patrickniyogitare28@gmail.com>
2024-10-05 21:22:01 +0200

    Added permissions

--- 100-clean_web_static.py
+++ 100-clean_web_static.py
@@ -8,7 +8,7 @@ fab -f 100-clean_web_static.py do_clean:number=2
 import os
 from fabric.api import *
 
-env.hosts = ['34.227.11.202', '52.90.153.53']
+env.hosts = ['34.204.52.54', '	23.20.105.26']
 
 
 def do_clean(number=0):
--- 3-deploy_web_static.py
+++ 3-deploy_web_static.py
@@ -9,7 +9,7 @@ execute: fab -f 3-deploy_web_static.py deploy -i ~/.ssh/id_rsa -u ubuntu
 from fabric.api import env, local, put, run
 from datetime import datetime
 from os.path import exists, isdir
-env.hosts = ['34.227.11.202', '52.90.153.53']
+env.hosts = ['18.212.225.47', '	34.224.29.127']
 
 
 def do_pack():
